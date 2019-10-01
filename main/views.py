a062f38311338e981d7a498bc9b146c99d615789
Anupam Chowdhury <anupam@localhost.localdomain>
2019-10-01 23:01:42 +0600

    first commit

---
+++ main/views.py
@@ -0,0 +1,20 @@
+from django.views.generic.edit import FormView
+from main import forms
+
+class ContactUsView(FormView):
+    template_name = "contact_form.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
+
+    def contact_us(request):
+        if request.method == 'POST':
+            form = forms.ContactForm(request.POST)
+            if form.is_valid():
+                form.send_mail()
+                return HttpResponseRedirect('/')
+        else:
+            form = forms.ContactForm()
+        return render(request, 'contact_form.html', {'form': form})
